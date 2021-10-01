886a522f98e10e1027e4621fbdcc22a2b8246bf0
Nono98 <noemie.plancherel@heig-vd.ch>
2021-10-01 16:30:31 +0200

    Initial commit

---
+++ src/main/java/ch/heigvd/amt/projet/App.java
@@ -0,0 +1,13 @@
+package ch.heigvd.amt.projet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

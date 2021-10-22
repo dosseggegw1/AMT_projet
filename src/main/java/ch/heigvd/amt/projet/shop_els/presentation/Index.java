d58773d88b36dcd90e65cc83544399c345c6db07
JL-HEIG <jean-luc.blanc2@heig-vd.ch>
2021-10-22 11:50:11 +0200

    les routes ne fonctionnent toujours pas

--- src/main/java/ch/heigvd/amt/projet/shop_els/presentation/Index.java
+++ src/main/java/ch/heigvd/amt/projet/shop_els/presentation/Index.java
@@ -12,6 +12,7 @@ public class Index extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("index.jsp").forward(request, response);
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
+
     }
 }

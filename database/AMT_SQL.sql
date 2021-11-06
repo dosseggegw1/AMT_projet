5db613466b709e283d27bf0414a7a90e2f50dfd3
Dosseggegw1 <gwen.dos@bluewin.ch>
2021-11-06 17:20:45 +0100

    Ajout de valeurs dans la DB

--- database/AMT_SQL.sql
+++ database/AMT_SQL.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : sam. 06 nov. 2021 à 16:33
+-- Généré le : sam. 06 nov. 2021 à 17:17
 -- Version du serveur : 5.6.48
 -- Version de PHP : 7.4.25
 
@@ -20,7 +20,9 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `AMT_SQL`
 --
-
+DROP SCHEMA IF EXISTS AMT_SQL;
+CREATE DATABASE IF NOT EXISTS `AMT_SQL` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `AMT_SQL`;
 -- --------------------------------------------------------
 
 --
@@ -32,21 +34,38 @@ CREATE TABLE `Article` (
   `name` varchar(50) NOT NULL,
   `description` text NOT NULL,
   `price` decimal(10,2) UNSIGNED DEFAULT NULL,
-  `imageURL` text NOT NULL,
-  `stock` smallint(6) UNSIGNED NOT NULL
+  `imageURL` varchar(255) DEFAULT 'default.jpg',
+  `stock` smallint(6) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `Article`
+--
+
+INSERT INTO `Article` (`idArticle`, `name`, `description`, `price`, `imageURL`, `stock`) VALUES
+(1, 'T-Shirt', 'T-shirt avec le logo du club. Taille unique.', '15.50', 'default.jpg', 15),
+(2, 'Jogging noire', 'Jogging de couleur noir avec le logo du club. Taille unique', '20.00', 'jogging.jpg', 15),
+(3, 'Porte-clés', 'Porte-clés en forme du logo du club. Dimension : 10 mm x 12 mm x 5 mm', '5.15', 'keychain.jpg', 5);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Article_Carte`
+-- Structure de la table `Article_Cart`
 --
 
-CREATE TABLE `Article_Carte` (
+CREATE TABLE `Article_Cart` (
   `fk_idArticle` int(11) DEFAULT NULL,
   `fk_idCart` int(11) DEFAULT NULL,
   `quantity` smallint(5) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `Article_Cart`
+--
+
+INSERT INTO `Article_Cart` (`fk_idArticle`, `fk_idCart`, `quantity`) VALUES
+(5, 1, 2),
+(2, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -59,6 +78,14 @@ CREATE TABLE `Article_Category` (
   `fk_idCategory` int(11) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Déchargement des données de la table `Article_Category`
+--
+
+INSERT INTO `Article_Category` (`fk_idArticle`, `fk_idCategory`) VALUES
+(1, 1),
+(2, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -68,6 +95,13 @@ CREATE TABLE `Article_Category` (
 CREATE TABLE `Cart` (
   `idCart` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `Cart`
+--
+
+INSERT INTO `Cart` (`idCart`) VALUES
+(1);
 
 -- --------------------------------------------------------
 
@@ -79,6 +113,14 @@ CREATE TABLE `Category` (
   `idCategory` int(11) NOT NULL,
   `name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `Category`
+--
+
+INSERT INTO `Category` (`idCategory`, `name`) VALUES
+(2, 'Accessoire'),
+(1, 'Vêtement');
 
 -- --------------------------------------------------------
 
@@ -92,6 +134,13 @@ CREATE TABLE `User` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Déchargement des données de la table `User`
+--
+
+INSERT INTO `User` (`idUser`, `fk_cart`) VALUES
+(1, 1);
+
+--
 -- Index pour les tables déchargées
 --
 
@@ -103,9 +152,9 @@ ALTER TABLE `Article`
   ADD UNIQUE KEY `name` (`name`);
 
 --
--- Index pour la table `Article_Carte`
+-- Index pour la table `Article_Cart`
 --
-ALTER TABLE `Article_Carte`
+ALTER TABLE `Article_Cart`
   ADD KEY `fk_idArticle` (`fk_idArticle`,`fk_idCart`),
   ADD KEY `fk_idCart` (`fk_idCart`);
 
@@ -144,49 +193,25 @@ ALTER TABLE `User`
 -- AUTO_INCREMENT pour la table `Article`
 --
 ALTER TABLE `Article`
-  MODIFY `idArticle` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idArticle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT pour la table `Cart`
 --
 ALTER TABLE `Cart`
-  MODIFY `idCart` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idCart` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `Category`
 --
 ALTER TABLE `Category`
-  MODIFY `idCategory` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idCategory` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `User`
 --
 ALTER TABLE `User`
-  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `Article_Carte`
---
-ALTER TABLE `Article_Carte`
-  ADD CONSTRAINT `Article_Carte_ibfk_1` FOREIGN KEY (`fk_idArticle`) REFERENCES `Article` (`idArticle`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `Article_Carte_ibfk_2` FOREIGN KEY (`fk_idCart`) REFERENCES `Cart` (`idCart`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `Article_Category`
---
-ALTER TABLE `Article_Category`
-  ADD CONSTRAINT `Article_Category_ibfk_1` FOREIGN KEY (`fk_idArticle`) REFERENCES `Article` (`idArticle`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `Article_Category_ibfk_2` FOREIGN KEY (`fk_idCategory`) REFERENCES `Category` (`idCategory`) ON DELETE SET NULL ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `User`
---
-ALTER TABLE `User`
-  ADD CONSTRAINT `User_ibfk_1` FOREIGN KEY (`fk_cart`) REFERENCES `Cart` (`idCart`) ON DELETE SET NULL ON UPDATE CASCADE;
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

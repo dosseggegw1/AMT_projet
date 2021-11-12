747d544b293f61b277edc1d6a949f923dcd8b35a
Nono98 <noemie.plancherel@heig-vd.ch>
2021-11-12 18:47:50 +0100

    UserTest OK

--- src/test/java/ch/heigvd/amt/projet/shop_els/model/UserTest.java
+++ src/test/java/ch/heigvd/amt/projet/shop_els/model/UserTest.java
@@ -11,14 +11,13 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class UserTest {
 
-
-
     @Test
     @Order(1)
-    public void shouldInsertUserData() throws IOException {
+    public void shouldInsertUserData()  {
         Session session = HibUtil.getSessionFactory().getCurrentSession();
         session.beginTransaction();
 
@@ -27,7 +26,7 @@ public class UserTest {
         User user = new User();
 
         cart.setIdCart(8);
-        user.setIdUser(19);
+        user.setIdUser(10);
 
         user.setFk_cart(cart);
         cart.setUser(user);
@@ -40,8 +39,48 @@ public class UserTest {
         Query query = session.createQuery("FROM User ORDER BY idUser DESC").setMaxResults(1);
         List<User> result = query.getResultList();
 
-        assertEquals(19, result.get(0).getIdUser());
+        assertEquals(10, result.get(0).getIdUser());
         session.getTransaction().commit();
         //session.close();
+    }
+
+    @Test
+    @Order(2)
+    public void shouldDeleteUserData() {
+
+        Session session = HibUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+
+        // Select datas that we inserted before
+        Query query = session.createQuery("FROM User ORDER BY idUser DESC").setMaxResults(1);
+        List<User> resultUser = query.getResultList();
+        User userToDel = resultUser.get(0);
+        query = session.createQuery("FROM Cart ORDER BY idCart DESC").setMaxResults(1);
+        List<Cart> resultCart = query.getResultList();
+        Cart cartToDel = resultCart.get(0);
+
+        // Delete datas that we added in the last test
+        query = session.createQuery("DELETE User where idUser=:idUser")
+                .setParameter("idUser", userToDel.getIdUser());
+        query.executeUpdate();
+
+        query = session.createQuery("DELETE Cart where idCart=:idCart")
+                .setParameter("idCart", cartToDel.getIdCart());
+        query.executeUpdate();
+
+
+        // Select last row of User and Cart to see if the datas were successfully deleted
+        query = session.createQuery("FROM User ORDER BY idUser DESC").setMaxResults(1);
+        resultUser = query.getResultList();
+        User userToVerify = resultUser.get(0);
+        query = session.createQuery("FROM Cart ORDER BY idCart DESC").setMaxResults(1);
+        resultCart = query.getResultList();
+        Cart cartToVerify = resultCart.get(0);
+
+        assertNotEquals(10, userToVerify.getIdUser());
+        assertNotEquals(8, cartToVerify.getIdCart());
+
+        session.getTransaction().commit();
+        session.close();
     }
 }

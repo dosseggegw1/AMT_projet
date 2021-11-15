27e7d0dd2db3b170b481dabd93d2e703351e71ec
Cassandre Wojciechowski <cassandre.wojciechowski@heig-vd.ch>
2021-11-15 14:03:37 +0100

    affichage de la liste d'articles sur la page index

--- src/main/java/ch/heigvd/amt/projet/shop_els/controller/Index.java
+++ src/main/java/ch/heigvd/amt/projet/shop_els/controller/Index.java
@@ -1,19 +1,34 @@
 package ch.heigvd.amt.projet.shop_els.controller;
 
+import ch.heigvd.amt.projet.shop_els.util.HibUtil;
+import org.hibernate.Session;
+
+import javax.persistence.Query;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/index")
 public class Index extends HttpServlet {
+    private Session session;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/index.jsp").forward(request, response);
+        response.setContentType("text/html");
 
+        session = HibUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        Query query = session.getNamedQuery("selectAllArticle");
+        List<Object[]> results = query.getResultList();
+        session.close();
+
+        request.setAttribute("articles", results);
+
+        request.getRequestDispatcher("/WEB-INF/view/admin/index.jsp").forward(request, response);
     }
 }
 

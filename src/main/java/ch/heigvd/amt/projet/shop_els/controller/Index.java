5211fe6e96802c46f0031af77252351ceb718af7
Cassandre Wojciechowski <cassandre.wojciechowski@heig-vd.ch>
2021-11-19 15:31:37 +0100

    cannot get categories for articles

--- src/main/java/ch/heigvd/amt/projet/shop_els/controller/Index.java
+++ src/main/java/ch/heigvd/amt/projet/shop_els/controller/Index.java
@@ -1,8 +1,7 @@
-
 package ch.heigvd.amt.projet.shop_els.controller;
+
 import ch.heigvd.amt.projet.shop_els.util.HibUtil;
 import org.hibernate.Session;
-
 import javax.persistence.Query;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,13 +20,30 @@ public class Index extends HttpServlet {
         response.setContentType("text/html");
         session = HibUtil.getSessionFactory().openSession();
         session.beginTransaction();
-        Query query = session.getNamedQuery("selectArticleAndCategory");
-        List<Object[]> resultsArticles = query.getResultList();
+        //Query query = session.getNamedQuery("selectArticleAndCategory");
+       // List<Object[]> resultsArticles = query.getResultList();
 
         Query second = session.getNamedQuery("selectAllCategory");
         List<Object[]> resultsCategories = second.getResultList();
 
+        Query bla = session.createSQLQuery("SELECT idArticle, Article.name, description, price, imageURL, stock, " +
+                "GROUP(Article_Category.fk_idCategory) AS categories FROM Article " +
+                "LEFT JOIN Article_Category ON Article_Category.fk_idArticle = Article.idArticle " +
+                "GROUP BY Article.idArticle");
+
+        List<Object[]> resultsArticles = bla.getResultList();
+
         session.close();
+
+        System.out.println("DEBUT LOG MADAFACKA");
+        for (Object[] obj : resultsArticles ) {
+            System.out.println("BLOUP");
+            for (Object wat : obj) {
+                System.out.println("WAT : " + wat);
+            }
+            System.out.println("BLAP");
+        }
+        System.out.println("FIN LOG MADAFACKA");
 
         request.setAttribute("articles", resultsArticles);
         request.setAttribute("categories", resultsCategories);

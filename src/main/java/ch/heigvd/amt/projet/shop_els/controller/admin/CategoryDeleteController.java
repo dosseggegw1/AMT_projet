df7cdbe06b298e46c8dea6ca4a9328c29b67c1e2
dosseggegw1 <gwendoline.dossegger@heig-vd.ch>
2021-12-06 15:05:22 +0100

    Affichage formulaire suppression d'une catégorie

--- src/main/java/ch/heigvd/amt/projet/shop_els/controller/admin/CategoryDeleteController.java
+++ src/main/java/ch/heigvd/amt/projet/shop_els/controller/admin/CategoryDeleteController.java
@@ -1,7 +1,10 @@
 package ch.heigvd.amt.projet.shop_els.controller.admin;
 
 import ch.heigvd.amt.projet.shop_els.access.ArticleCategoryDao;
+import ch.heigvd.amt.projet.shop_els.access.ArticleDao;
 import ch.heigvd.amt.projet.shop_els.access.CategoryDao;
+import ch.heigvd.amt.projet.shop_els.model.Article;
+import ch.heigvd.amt.projet.shop_els.model.Category;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,11 +21,28 @@ import java.util.Set;
 public class CategoryDeleteController extends HttpServlet {
     private final CategoryDao categoryDao = new CategoryDao();
     private final ArticleCategoryDao articleCategoryDao = new ArticleCategoryDao();
+    private final ArticleDao articleDao = new ArticleDao();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        int idCategory = Integer.parseInt(request.getParameter("id"));
+        Category category = categoryDao.get(idCategory);
+
+        // TODO : récupérer liste des articles selon la catégorie données
+        List<Article> articles = articleDao.getAll();
+        request.setAttribute("articles", articles);
+        request.setAttribute("category", category);
+
+        request.getRequestDispatcher("/WEB-INF/view/admin/categoryDelete.jsp").forward(request, response);
+    }
+
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        int idCategory =  Integer.parseInt(request.getParameter("id"));
+        //à check si tu récupères bien l'id mais ca devrait être ok via le formulaire
+        int idCategory =  Integer.parseInt(request.getParameter("idCategory"));
 
         //String messageError = "";
 

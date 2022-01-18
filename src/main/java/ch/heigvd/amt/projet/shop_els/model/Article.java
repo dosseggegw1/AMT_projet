1f0b5b13e681ac1245e60a2cafe2a8cedde47ffb
Lindwing <rui.lopesgouveia@heig-vd.ch>
2022-01-19 00:01:27 +0100

    fix dependencie error

--- src/main/java/ch/heigvd/amt/projet/shop_els/model/Article.java
+++ src/main/java/ch/heigvd/amt/projet/shop_els/model/Article.java
@@ -4,6 +4,7 @@ import org.apache.commons.io.FilenameUtils;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static ch.heigvd.amt.projet.shop_els.util.Constants.*;

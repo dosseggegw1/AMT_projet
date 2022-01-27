7f1105935b5b2922241dca29d8df3e5ecdb88859
Cassandre Wojciechowski <cassandre.wojciechowski@heig-vd.ch>
2022-01-27 16:18:26 +0100

    refactoring en cours

--- src/main/java/ch/heigvd/amt/projet/shop_els/access/ArticleCategoryDao.java
+++ src/main/java/ch/heigvd/amt/projet/shop_els/access/ArticleCategoryDao.java
@@ -11,6 +11,10 @@ import java.util.List;
 public class ArticleCategoryDao implements Dao<Article_Category> {
     private Session session;
 
+    /**
+     * Sauvegarde d'un objet de type Article_Category dans la base de données
+     * @param article_category objet de type Article_Category à sauvegarder
+     */
     @Override
     public void save(Article_Category article_category) {
         session = HibUtil.getSessionFactory().openSession();
@@ -22,6 +26,10 @@ public class ArticleCategoryDao implements Dao<Article_Category> {
         session.close();
     }
 
+    /**
+     * Mise à jour d'un objet de type Article_Category dans la base de données
+     * @param article_category objet de type Article_Category à modifier
+     */
     @Override
     public void update(Article_Category article_category) {
         session = HibUtil.getSessionFactory().openSession();
@@ -35,6 +43,11 @@ public class ArticleCategoryDao implements Dao<Article_Category> {
         session.close();
     }
 
+    /**
+     * Récupération d'un objet de type Article_Category depuis la base de données
+     * @param id Identifiant de l'objet à récupérer
+     * @returns un objet de type Article_Category
+     */
     @Override
     public Article_Category get(int id) throws DaoException {
         session = HibUtil.getSessionFactory().openSession();
@@ -54,6 +67,11 @@ public class ArticleCategoryDao implements Dao<Article_Category> {
         return null;
     }
 
+    /**
+     * Suppression d'un objet de type Article_Category selon un identifiant
+     * @param id identifiant de l'objet à supprimer
+     * @throws DaoException si la suppression n'a pas été effectuée correctement
+     */
     @Override
     public void delete(int id) throws DaoException {
         session = HibUtil.getSessionFactory().openSession();
@@ -71,17 +89,11 @@ public class ArticleCategoryDao implements Dao<Article_Category> {
         }
     }
 
-    public boolean checkIfHasArticles(int idCategory) {
-        session = HibUtil.getSessionFactory().openSession();
-        session.beginTransaction();
-
-        Category category = session.get(Category.class, idCategory);
-        List list = session.getNamedQuery("selectArticleByCategory").setParameter("cat", category).getResultList();
-
-        session.close();
-        return !list.isEmpty();
-    }
-
+    /**
+     * Récupération des articles faisant partie d'une catégorie
+     * @param idCategory identifiant de la catégorie dont on veut récupérer les articles
+     * @return une liste d'articles faisant partie de la catégorie
+     */
     public List<Article> getArticlesById(int idCategory) {
         session = HibUtil.getSessionFactory().openSession();
         session.beginTransaction();
@@ -93,6 +105,10 @@ public class ArticleCategoryDao implements Dao<Article_Category> {
         return list;
     }
 
+    /**
+     * Récupération de tous les objets de types Article_Category
+     * @return une liste d'objets
+     */
     public List<Object[]> getAllArticlesCategories() {
         session = HibUtil.getSessionFactory().openSession();
         session.beginTransaction();
@@ -103,6 +119,10 @@ public class ArticleCategoryDao implements Dao<Article_Category> {
         return resultsArticles;
     }
 
+    /**
+     * Récupération des catégories liées à au moins un article
+     * @return une liste de noms de catégories
+     */
     public List<String> getCategoriesLinked() {
         session = HibUtil.getSessionFactory().openSession();
         session.beginTransaction();
@@ -113,6 +133,11 @@ public class ArticleCategoryDao implements Dao<Article_Category> {
         return resultsCategoriesLinked;
     }
 
+    /**
+     * Récupération des noms de catégories liées à un article précis
+     * @param id identifiant de l'article dont on souhaite récupérer les catégories liées
+     * @return une liste de noms de catégories
+     */
     public List<String> getCategoriesNameByArticleId(int id) {
         session = HibUtil.getSessionFactory().openSession();
         session.beginTransaction();
@@ -123,6 +148,11 @@ public class ArticleCategoryDao implements Dao<Article_Category> {
         return resultsCategories;
     }
 
+    /**
+     * Récupération des identifiants des catégories liées à un article précis
+     * @param id identifiant de l'article dont on souhaite récupérer les catégories liées
+     * @return une liste d'identifiants sous forme d'entiers
+     */
     public List<Integer> getCategoriesIdByArticleId(int id){
         session = HibUtil.getSessionFactory().openSession();
         session.beginTransaction();
@@ -133,6 +163,12 @@ public class ArticleCategoryDao implements Dao<Article_Category> {
         return resultsCategories;
     }
 
+    /**
+     * Récupération de l'identifiant de l'objet de type Article_Category selon les identifiants d'un article et d'une catégorie
+     * @param idArticle identifiant de l'article
+     * @param idCategory identifiant de la catégorie
+     * @return une liste d'identifiants sous forme d'entiers
+     */
     public Integer getArticleCategoryId(int idArticle, int idCategory) {
         session = HibUtil.getSessionFactory().openSession();
         session.beginTransaction();
--- src/main/java/ch/heigvd/amt/projet/shop_els/access/CartDao.java
+++ src/main/java/ch/heigvd/amt/projet/shop_els/access/CartDao.java
@@ -3,12 +3,15 @@ package ch.heigvd.amt.projet.shop_els.access;
 import ch.heigvd.amt.projet.shop_els.model.Cart;
 import ch.heigvd.amt.projet.shop_els.util.HibUtil;
 import org.hibernate.Session;
-
 import java.util.List;
 
 public class CartDao implements Dao<Cart> {
     private Session session;
 
+    /**
+     *
+     * @param cart
+     */
     @Override
     public void save(Cart cart) {
         session = HibUtil.getSessionFactory().openSession();
